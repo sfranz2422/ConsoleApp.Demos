5d331ed1ee404c6780f9f1d1573eebbf8899ad65
Stephen Franz <stephenfranz22@gmail.com>
2026-01-14 14:12:35 -0500

    Conditionals

---
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Please enter student's grade: ");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+if (grade >= 90)
+{
+    Console.WriteLine("Student's grade is A");
+}
+else if (grade >= 80)
+{
+    Console.WriteLine("Student's grade is B");
+}
+else if (grade >= 70)
+{
+    Console.WriteLine("Student's grade is C");
+}
+else if (grade >= 60)
+{
+    Console.WriteLine("Student's grade is D");
+}
+else
+{
+    Console.WriteLine("Student's grade is F");
+}
+
+string passStatus = grade < 50 ? "Fail" : "Pass";
+Console.WriteLine(passStatus);
+
+
