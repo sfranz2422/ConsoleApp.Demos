8839af76d880d0a0747a81eb24d8a7ac4a4030e8
Stephen Franz <stephenfranz22@gmail.com>
2026-01-15 13:06:41 -0500

    improve calculator and added fibonacci

---
+++ ConsoleApp.ForLoops/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("How many times would you like to print \"Hello World\": ");
+int lineNumber = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 0; i < lineNumber; i++)
+{
+    Console.WriteLine($"Hello World! {i}");
+}
+
+
+int counter = 10;
+
+
+while (counter > 0)
+{
+    Console.WriteLine($"Counter: {counter}");
+    counter--;
+}
+
+
+Console.WriteLine("Enter numbers to be summed:");
+int sum = 0;
+int num = 0;
+while (num != -1)
+{
+
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num != -1)
+    {
+        sum += num;
+    }
+}
+Console.WriteLine($"The sum is {sum}");
+
+
+
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,40 +1,84 @@
-﻿Console.WriteLine("Welcome to the sample calculator");
+﻿bool keepGoing = true;
 
-
-
-Console.Write("Please enter the first number: ");
-decimal num1 = Convert.ToDecimal(Console.ReadLine());
-
-Console.Write("Please enter the secone number: ");
-decimal num2 = Convert.ToDecimal(Console.ReadLine());
-
-Console.Write("What operation would you like to perform? (+,-,*,/): ");
-string operation = Console.ReadLine();
-
-decimal answer = 0;
-switch (operation)
+while (keepGoing == true)
 {
-    case "+":
-        answer = num1 + num2;
-        break;
 
-    case "-":
-        answer = num1 - num2;
-        break;
+    Console.WriteLine("Welcome to the sample calculator");
 
-    case "*":
-        answer = num1 * num2;
-        break;
 
-    case "/":
-        answer = num1 / num2;
-        break;
 
-    default:
-        Console.WriteLine("Invalid Input");
-        break;
+    Console.Write("Please enter the first number or type \"exit\" to exit: ");
+    string ans = Console.ReadLine();
 
+
+    if (ans == "exit")
+    {
+        keepGoing = false;
+        break;
+    }
+
+    int num1 = Convert.ToInt32(ans);
+
+
+
+    Console.Write("Please enter the second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("What operation would you like to perform? (+,-,*,/,fib)\n \"fib\" will give the nth digit of the fibonacci sequence to the second number: ");
+    string operation = Console.ReadLine();
+
+    int answer = 0;
+    switch (operation)
+    {
+        case "+":
+            answer = num1 + num2;
+            break;
+
+        case "-":
+            answer = num1 - num2;
+            break;
+
+        case "*":
+            answer = num1 * num2;
+            break;
+
+        case "/":
+            answer = num1 / num2;
+            break;
+
+        case "fib":
+            int a = 1;
+            int b = 1;
+
+            for (int i = 0; i < (num2 - 2); i++)
+            {
+                answer = a + b;
+                if (i % 2 == 0)
+                {
+                    a = answer;
+                }
+                else
+                {
+                    b = answer;
+                }
+
+
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid Input");
+            break;
+
+    }
+    if (operation != "fib")
+    {
+        Console.WriteLine($"{num1} {operation} {num2} = {answer}");
+
+    }
+    else
+    {
+        Console.WriteLine($"The {num2}th digit of the fib sequence is {answer}");
+
+    }
 }
-
-Console.WriteLine($"{num1} {operation} {num2} = {answer}");
 
