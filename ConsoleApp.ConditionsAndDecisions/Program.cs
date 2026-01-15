31de4a2a5a7888bbe4c95a336a7f34587a5300a9
Stephen Franz <stephenfranz22@gmail.com>
2026-01-15 11:02:49 -0500

    Calculator project

--- ConsoleApp.ConditionsAndDecisions/Program.cs
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Please enter student's grade: ");
+﻿using System.Net.Http.Headers;
+
+Console.Write("Please enter student's grade: ");
 int grade = Convert.ToInt32(Console.ReadLine());
 
 if (grade >= 90)
@@ -25,4 +27,35 @@ else
 string passStatus = grade < 50 ? "Fail" : "Pass";
 Console.WriteLine(passStatus);
 
+Console.Write("Enter a number (1-7) to get the corresponding day of the week: ");
+int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+switch (dayOfTheWeek)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Invalid day");
+        break;
 
+
+
+}
---
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Welcome to the sample calculator");
+
+
+
+Console.Write("Please enter the first number: ");
+decimal num1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Please enter the secone number: ");
+decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("What operation would you like to perform? (+,-,*,/): ");
+string operation = Console.ReadLine();
+
+decimal answer = 0;
+switch (operation)
+{
+    case "+":
+        answer = num1 + num2;
+        break;
+
+    case "-":
+        answer = num1 - num2;
+        break;
+
+    case "*":
+        answer = num1 * num2;
+        break;
+
+    case "/":
+        answer = num1 / num2;
+        break;
+
+    default:
+        Console.WriteLine("Invalid Input");
+        break;
+
+}
+
+Console.WriteLine($"{num1} {operation} {num2} = {answer}");
+
