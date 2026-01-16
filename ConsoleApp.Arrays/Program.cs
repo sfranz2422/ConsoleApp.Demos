271918af9515d12256070647156aaf808c558118
Stephen Franz <stephenfranz22@gmail.com>
2026-01-16 12:38:32 -0500

    lists

---
+++ ConsoleApp.Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//this is an array
+int[] grades = new int[5];
+//now i need 5 elements
+
+grades[0] = 10;
+
+
+Console.WriteLine(grades.ToString());
+
+for (int i = 0; i<grades.Length; i++)
+{
+    Console.WriteLine("Enter a Grade: ");
+    int grade = Convert.ToInt32(Console.ReadLine());
+    grades[i] = grade;
+}
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.WriteLine(grades[i]);
+
+}
+
+// variable size array
+string[] studentNames = { "steve", "mel", "luca", "gia" };
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine("Enter a name: ");
+    string name = Console.ReadLine();
+    studentNames[i] = name;
+}
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine(studentNames[i]);
+
+}
---
+++ ConsoleApp.Lists/Program.cs
@@ -0,0 +1,28 @@
+﻿//declare a list
+
+
+List <string> myList = new List<string>();
+//var grades = new List<string>();
+//List<int> grades = new();
+
+List<int> grades = new List<int>();
+int grade = 0;
+
+
+
+
+myList.Add("steve");
+
+do
+{
+    Console.WriteLine("Enter a grade: ");
+    grade = Convert.ToInt32(Console.ReadLine());
+    grades.Add(grade);
+
+
+} while (grade != -1);
+
+foreach(int g in grades)
+{
+    Console.WriteLine($"{g}");
+}
